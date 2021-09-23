000a40fe05b21b5b0f5c9fa085a9939152cd41c1
Mateus Campos <mateus.camphos2000@gmail.com>
2021-09-23 16:47:49 -0300

    Visitante é impedido de cadastrar imóvel já existente

--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -1,3 +1,4 @@
 class PropertyType < ApplicationRecord
   validates :name, presence: { message: 'Tipo de imóvel não cadastrado, preencha todos os campos' }
+  validates :name, uniqueness: {message: 'Tipo de imóvel já existente.'}
 end
